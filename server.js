6888c47980d63ac4b55f921737b36a2ac9cb276c
Annaivv <annashalya@gmail.com>
2023-10-18 22:32:59 +0300

    postgres connection

--- server.js
+++ server.js
@@ -1,47 +1,51 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+const { Client } = require("pg");
+const cors = require("cors");
 const app = require("./app");
-const { DB_HOST } = process.env;
+const port = 4000;
+require("dotenv").config();
+const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
 
-mongoose.set("strictQuery", true);
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(4000, () => console.log("Server is running"));
-  })
-  .catch((err) => {
-    console.log(err.message);
-    process.exit(1);
-  });
+// const { DB_HOST } = process.env;
+
+// mongoose.set("strictQuery", true);
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => {
+//     app.listen(4000, () => console.log("Server is running"));
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//     process.exit(1);
+//   });
 
 // const { Client } = require("pg");
 // const cors = require("cors");
 // const express = require("express");
 // const app = express();
-// const port = 4000;
-// require("dotenv").config();
 
 // const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
 
-// const client = new Client({
-//   host: DB_HOST,
-//   user: DB_USERNAME,
-//   port: 5432,
-//   password: DB_PASSWORD,
-//   database: DB_NAME,
-// });
+const client = new Client({
+  host: DB_HOST,
+  user: DB_USERNAME,
+  port: 5432,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+});
 
-// client.connect();
+client.connect();
 
-// client.query("Select * from deals", (err, res) => {
-//   if (!err) {
-//     console.log(res.rows);
-//   } else {
-//     console.log(err.message);
-//   }
-//   client.end();
-// });
-// app.use(cors());
+client.query("Select * from deals", (err, res) => {
+  if (!err) {
+    console.log(res.rows);
+  } else {
+    console.log(err.message);
+  }
+  client.end();
+});
+app.use(cors());
 
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
