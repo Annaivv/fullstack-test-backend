ba966ebaf5f5f2b18146a9b7c6255d9de0cdf1b3
Annaivv <annashalya@gmail.com>
2023-10-18 19:03:16 +0300

    dev working version with MongoDB

--- controllers/deals.js
+++ controllers/deals.js
@@ -7,16 +7,6 @@ const getAll = async (req, res, next) => {
   res.json(result);
 };
 
-const getById = async (req, res, next) => {
-  const { id } = req.params;
-  const result = await Deal.findById(id);
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  res.json(result);
-};
-
 module.exports = {
   getAll: ctrlWrapper(getAll),
-  getById: ctrlWrapper(getById),
 };
--- routes/api/deals.js
+++ routes/api/deals.js
@@ -1,12 +1,9 @@
 const express = require("express");
 
 const ctrl = require("../../controllers/deals");
-const { isValidId } = require("../../middlewares");
 
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
-
-router.get("/:id", isValidId, ctrl.getById);
 
 module.exports = router;
