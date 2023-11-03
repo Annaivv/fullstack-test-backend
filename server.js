63abfbf6f07a05fe2bca5417265e38bffb2a9e14
Annaivv <annashalya@gmail.com>
2023-11-03 18:58:35 +0200

    strictQuery removed

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const app = require("./app");
 
 const { DB_HOST, PORT } = process.env;
 
-mongoose.set("strictQuery", true);
+//mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT, () => console.log("Server is running"));
+    app.listen(PORT, () => {
+      console.log("Server is running");
+      console.log(DB_HOST);
+    });
   })
   .catch((err) => {
     console.log(err.message);
